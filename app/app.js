0b0536f576aac863a9d741773b85509d16734a98
Andrey Grudtsin <agrudtsin@gmail.com>
2014-12-14 16:14:46 +0200

    before clear HTML

--- app/app.js
+++ app/app.js
@@ -3,4 +3,11 @@
 angular.module('myApp', []).
     controller('mainCtrl', ['$scope', function($scope) {
         $scope.isDefined = true;
+        $scope.nativeLanguagePhrase = "Меня зовут Коля";
+
+        $scope.anotherRedText = $scope.nativeLanguagePhrase;
+        $scope.redText = "";
+        $scope.grayText = "";
+        $scope.userText = "";
+        $scope.nativeLanguagePhrase = "Меня зовут Коля";
     }]);
