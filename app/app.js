0b8d15016cbcc14c0968f91b1c8e3b244b205e4f
agrudtsin1 <grudtsin.andrey@retalindustries.eu>
2014-12-17 16:29:48 +0000

    basic underlined text added

--- app/app.js
+++ app/app.js
@@ -3,17 +3,49 @@
 angular.module('myApp', []).
     factory('phrases', function () {
         return {
+            commands : [],
+            pushCommand : function(command){
+                return this.commands.push(command);
+            },
+            flushCommands : function(){
+                 this.commands = []
+                 return this.commands;
+            },
             currentPhrase : {
                 text: {
                     ru: "Меня зовут Коля",
                     en: "My name is Nikolay"
                 }
             },
-
+            
+            makeUnderlinedText : function(userText){
+                return underlineUserText(userText);
+                
+                function underlineUserText(userText){
+                    var underlinedText = ""
+                    for (var curChar = 0; curChar < userText.length; curChar +=1 ) {
+                        if(userText[curChar] == " "){
+                            underlinedText += " " ;
+                        } else {
+                            underlinedText += underlineOrPunctuationМark(userText[curChar]) ;
+                        }
+                    };  
+                    return underlinedText;
+                };   
+                
+                function underlineOrPunctuationМark(character){
+                    var punctuationMarks = [',','.',';',':','?','!'];
+                    if(punctuationMarks.indexOf(character) !== -1){
+                        return character
+                    } else {
+                        return "_";    
+                    }
+                };
+            },
+            
             getCurrent : function(){
                 return this.currentPhrase;
             }
-
         }
     }).
     directive('ngEnter', function () {  //http://stackoverflow.com/questions/17470790/how-to-use-a-keypress-event-in-angularjs
@@ -31,36 +63,34 @@ angular.module('myApp', []).
     }).
     controller('mainCtrl', ['$scope', 'phrases', function($scope, phrases) {
         $scope.isDefined = true;
-
         $scope.nativeLanguagePhrase = phrases.getCurrent().text.ru;
-
+        $scope.userText = "";
+        $scope.anotherRedText = "";
         $scope.blueText = "";
         $scope.redText = "";
-        $scope.underlinesText = "";
+        $scope.underlinesText = "";// phrases.makeUnderlinedText($scope.userText);
         $scope.grayText = "";
-        $scope.userText = "";
+        
         $scope.nativeLanguagePhrase = "Меня зовут Коля";
 
         $scope.onUserTextChange = function (){
             //underline every character
-            $scope.underlinesText = "";
-            for(var currentChar in $scope.userText){
-                if($scope.userText[currentChar] === " "){
-                    $scope.underlinesText += " ";
-                } else {
-                    $scope.underlinesText += "_";
-                }
-            }
-            
-            
+            $scope.underlinesText = phrases.makeUnderlinedText($scope.userText);
+            //for(var currentChar in $scope.userText){
+            //    if($scope.userText[currentChar] === " "){
+            //        $scope.underlinesText += " ";
+            //    } else {
+            //        $scope.underlinesText += "_";
+            //    }
+            //}
+            $scope.redText = $scope.userText;
 
+        };
 
         $scope.onUserTextEnter = function () {
             console.log($scope.userText);
+        };
+        $scope.FocusOnInput = function() {
+            document.getElementById("userText").focus();
         }
-        markRedText(mText) = function {
-            return "<span style="+Символ(34)+"color:red"+Символ(34)+">"+mText+"</span>";
-        }
-
-        
     }]);
--- app/app_test.js
+++ app/app_test.js
@@ -10,9 +10,76 @@ describe('myApp stress tests', function() {
         it('jasmine stress test', function() {
             expect(true).toBeTruthy();
         });
+    });
 
-
-
+    
+     describe('phrases service test', function(){
+        var phrases;
+        
+        beforeEach(function() {
+            module('myApp');
+            inject(function($injector) {
+                phrases = $injector.get('phrases');
+            });
+        });
+        
+        it('phrases service stress test', function() {
+            expect(phrases).toBeDefined();
+        });
+        
+        describe('phrases service unit tests', function(){
+            it('it should append enters to the commands array', function() {
+                expect(phrases.commands).toEqual(jasmine.any(Object));
+                expect(phrases.commands).not.toContain("Enter");
+            
+                phrases.pushCommand("Enter");
+                expect(phrases.commands).toContain("Enter");
+                expect(phrases.commands.length).toEqual(1);
+                
+                phrases.pushCommand("Enter");
+                phrases.pushCommand("Enter");
+                expect(phrases.commands.length).toEqual(3);
+                
+                phrases.pushCommand("Enter");
+                expect(phrases.commands.length).toEqual(4);
+            });
+            it('it should flush the commands array', function() {
+                expect(phrases.commands).toEqual(jasmine.any(Object));
+                
+                phrases.pushCommand("Enter");
+                phrases.pushCommand("Enter");
+                phrases.pushCommand("Enter");
+                expect(phrases.commands.length).toEqual(3);
+                
+                
+                phrases.flushCommands();
+                expect(phrases.commands.length).toEqual(0);
+            });
+            
+        });
+        describe('Тесты логики формирования подчеркнутого текста', function(){
+            it('Каждое пользовательское слово должно быть подчеркнуто', function() {
+                expect(phrases.makeUnderlinedText("Вася", [])).toEqual("____");
+                expect(phrases.makeUnderlinedText("Ва", [])).toEqual("__");
+                expect(phrases.makeUnderlinedText("Вася ПМ", [])).toEqual("____ __");
+                expect(phrases.makeUnderlinedText(" Вася", [])).toEqual(" ____");
+                expect(phrases.makeUnderlinedText(" ", [])).toEqual(" ");
+                expect(phrases.makeUnderlinedText("", [])).toEqual("");
+            });
+            it('Не должны подчеркиваться знаки пунктуации', function() {
+                expect(phrases.makeUnderlinedText("Вася, Коля", [])).toEqual("____, ____");
+                expect(phrases.makeUnderlinedText("Вася,.Коля", [])).toEqual("____,.____");
+                expect(phrases.makeUnderlinedText(".", [])).toEqual(".");
+                expect(phrases.makeUnderlinedText("12.", [])).toEqual("__.");
+                expect(phrases.makeUnderlinedText(".,?!;: q", [])).toEqual(".,?!;: _");
+                
+                
+            });
+            it('Когда один раз нажимем Enter - подчеркивается слово', function() {
+            
+            });
+        });
+        
     });
 
     describe('myApp controller', function(){
